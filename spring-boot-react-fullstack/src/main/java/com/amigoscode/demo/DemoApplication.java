bf8c4f7fe666b4d8fdf1319a83d6b5729df0e856
Diego Pinzon <dipinzon@hotmail.com>
2023-06-27 08:36:37 -0400

    Add files via upload

---
+++ spring-boot-react-fullstack/src/main/java/com/amigoscode/demo/DemoApplication.java
@@ -0,0 +1,15 @@
+package com.amigoscode.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+
+        String message = "Hello How are you";
+        System.out.println(message);
+    }
+}
