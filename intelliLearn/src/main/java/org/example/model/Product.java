14c7277b910a394b0e34878d1bcbeee51df2b7f6
dipinzon <dipinzon@hotmail.com>
2023-06-27 08:50:42 -0400

    Changes

---
+++ intelliLearn/src/main/java/org/example/model/Product.java
@@ -0,0 +1,12 @@
+package org.example.model;
+
+public class Product {
+     private String name;
+     public Product (String name){
+         this.name = name;
+     }
+
+    public String getName() {
+        return name;
+    }
+}
